fdd8470836f75b902cabe5cec9fdadb2185d47e5
Jonathan Chosson <catstrike@gmail.com>
2021-01-26 11:32:47 +0100

    [Back]app.js :zap: Premier lancement du back

--- back/app.js
+++ back/app.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const bobyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// const stuffRoutes = require('./routes/stuff');
+// const userRoutes = require('./routes/user');
 
 const app = express();
 
-module.export = app;
+mongoose.connect('mongodb+srv://admin:pkoipa@cluster0.0fbz5.mongodb.net/projet6?retryWrites=true&w=majority', 
+    { useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(() =>console.log('Connexion à MongoDB réussi'))
+    .catch(() =>console.log('Connexion à MongoDb échouée'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bobyParser.json());
+
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+// app.use('/api/stuff', stuffRoutes);
+// app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
