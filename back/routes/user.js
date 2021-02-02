8c58207466ac8d2eebf91c6d8821dcaabdf217f2
Jonathan Chosson <catstrike@gmail.com>
2021-02-02 17:41:41 +0100

    [Back] Rate limit :sparkles: mise en place de rate limit

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const limiter =require ('../middleware/express-limit')
 
 //lier les routes au controllers
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', limiter, userCtrl.signup);
+router.post('/login', limiter, userCtrl.login);
 
 
 module.exports = router;
